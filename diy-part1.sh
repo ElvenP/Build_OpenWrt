1138d06ac1790cc67649c466232e55586c6f0724
ElvenP <66731105+ElvenP@users.noreply.github.com>
2022-10-21 13:50:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,6 +4,8 @@
 #src-git small https://github.com/kenzok8/small
 #EOF
 
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
  
-git clone https://github.com/kenzok8/small.git package/small
+#git clone https://github.com/kenzok8/small.git package/small
+
+git clone https://github.com/kenzok8/small-package package/small-package
